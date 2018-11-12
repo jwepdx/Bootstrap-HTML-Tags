597ef7ea5c61b97315148a0f0b51c25a15dd9ae7
Jacob Evans <37279570+jwepdx@users.noreply.github.com>
2018-11-11 22:11:06 -0800

    try some things

--- src/js/card.js
+++ src/js/card.js
@@ -2,134 +2,137 @@ class Card extends HTMLElement {
   constructor() {
     super()
     const shadow = this.attachShadow({
-      mode: 'open'
+      mode: "open"
     })
     // Style
-    const style = document.createElement('link')
-    style.setAttribute('rel', 'stylesheet')
-    style.setAttribute('href', '../dist/css/card.min.css')
+    const style = document.createElement("link")
+    style.setAttribute("rel", "stylesheet")
+    style.setAttribute("href", "../dist/css/card.min.css")
     shadow.appendChild(style)
     // Container
-    const container = document.createElement('div')
-    container.classList.add('card')
+    const container = document.createElement("div")
+    container.classList.add("card")
     // Get Different Attributes
-    if (typeof this.getElementsByTagName('img')[0] !== "undefined" || this.getElementsByTagName('img')[0] !== null); {
-      const img = this.getElementsByTagName('img')[0]
+    const img = this.getElementsByTagName("img")[0];
+    if(typeof img == "undefined" || img == null) {
+      const img = null;
     }
-    if (typeof this.getElementsByTagName('header')[0].innerHTML !== "undefined" || this.getElementsByTagName('header')[0].innerHTML !== null); {
-      const header = this.getElementsByTagName('header')[0].innerHTML
+    const header = this.getElementsByTagName("h4")[0].innerHTML;
+    if(typeof header == "undefined" || header == null) {
+      const header = null;
     }
-    if (typeof this.getElementsByTagName('h5')[0].innerHTML !== "undefined" || this.getElementsByTagName('h5')[0].innerHTML !== null); {
-      const title = this.getElementsByTagName('h5')[0].innerHTML
+    if(typeof this.getElementsByTagName("h5")[0].innerHTML !== "undefined" || this.getElementsByTagName("h5")[0].innerHTML !== null) {
+      const title = this.getElementsByTagName("h5")[0].innerHTML
     }
-    if (typeof this.getElementsByTagName('h6')[0].innerHTML !== "undefined" || this.getElementsByTagName('h6')[0].innerHTML !== null); {
-      const subTitle = this.getElementsByTagName('h6')[0].innerHTML
+    let subTitle = this.getElementsByTagName("h6")[0].innerHTML;
+    if(subTitle == undefined || subTitle == null) {
+      const subTitle = null;
     }
-    if (this.getElementsByTagName('p')[0].innerHTML != null || undefined); {
-      const text = this.getElementsByTagName('p')[0].innerHTML
+    if(this.getElementsByTagName("p")[0].innerHTML != null || undefined) {
+      const text = this.getElementsByTagName("p")[0].innerHTML
     }
-    if (this.getElementsByTagName('footer')[0].innerHTML != null || undefined); {
-      const footer = this.getElementsByTagName('footer')[0].innerHTML
+    if(this.getElementsByTagName("footer")[0].innerHTML != null || undefined) {
+      const footer = this.getElementsByTagName("footer")[0].innerHTML
     }
-    if (this.getElementsByTagName('ul') != null || undefined); {
-      const listGroup = this.getElementsByTagName('ul')
+    if(this.getElementsByTagName("ul") != null || undefined) {
+      const listGroup = this.getElementsByTagName("ul")
     }
-    if (this.getElementsByTagName('a') != null || undefined); {
-      const links = this.getElementsByTagName('a')
+    if(this.getElementsByTagName("a") != null || undefined) {
+      const links = this.getElementsByTagName("a")
     }
-    if (this.getElementsByTagName('button') != null || undefined); {
-      const buttons = this.getElementsByTagName('button')
+    if(this.getElementsByTagName("button") != null || undefined) {
+      const buttons = this.getElementsByTagName("button")
     }
     // Images
-    if (img !== null) {
-      const imgSource = img.getAttribute('src')
-      const imgAlt = img.getAttribute('alt')
-      const imageContainer = document.createElement('img')
-      imageContainer.setAttribute('alt', imgAlt)
-      imageContainer.classList.add('card-img-top')
-      imageContainer.setAttribute('src', imgSource)
+    if(img !== null) {
+      const imgSource = img.getAttribute("src")
+      const imgAlt = img.getAttribute("alt")
+      const imageContainer = document.createElement("img")
+      imageContainer.setAttribute("alt", imgAlt)
+      imageContainer.classList.add("card-img-top")
+      imageContainer.setAttribute("src", imgSource)
       container.appendChild(imageContainer)
     }
     // Header
-    if (header !== null) {
-      const headerArea = document.createElement('div')
-      headerArea.classList.add('card-header')
+    if(header !== null) {
+      const headerArea = document.createElement("div")
+      headerArea.classList.add("card-header")
       headerArea.textContent = header
       container.appendChild(headerArea)
     }
     // Body
-    const body = document.createElement('div')
-    body.classList.add('card-body')
+    const body = document.createElement("div")
+    body.classList.add("card-body")
     container.appendChild(body)
     // Title
-    if (title !== null) {
-      const titleContainer = document.createElement('h5')
-      titleContainer.classList.add('card-title')
+    if(title !== null) {
+      const titleContainer = document.createElement("h5")
+      titleContainer.classList.add("card-title")
       titleContainer.textContent = title
       body.appendChild(titleContainer)
     }
     // Sub-Title
-    if (subTitle !== null) {
-      const subTitleContainer = document.createElement('h6')
-      subTitleContainer.classList.add('card-subtitle', 'mb-2', 'text-muted')
+    if(subTitle !== null) {
+      const subTitleContainer = document.createElement("h6")
+      subTitleContainer.classList.add("card-subtitle", "mb-2", "text-muted")
       subTitleContainer.textContent = subTitle
       body.appendChild(subTitleContainer)
     }
     // Text
-    if (text !== null) {
-      const textContainer = document.createElement('p')
+    if(text !== null) {
+      const textContainer = document.createElement("p")
       textContainer.textContent = text
       body.appendChild(textContainer)
     }
     // List Group
-    if (listGroup !== null) {
-      $('bs-card ul li').addClass('list-group-item')
-      const listGroupItems = this.getElementsByTagName('ul')[0].innerHTML
-      const listContainer = document.createElement('ul')
-      listContainer.classList.add('list-group', 'list-group-flush')
+    if(listGroup !== null) {
+      $("bs-card ul li").addClass("list-group-item")
+      const listGroupItems = this.getElementsByTagName("ul")[0].innerHTML
+      const listContainer = document.createElement("ul")
+      listContainer.classList.add("list-group", "list-group-flush")
       listContainer.innerHTML = listGroupItems
       container.appendChild(listContainer)
     }
     // Links
-    if (links !== null) {
-      const linkContainer = document.createElement('div')
-      linkContainer.classList.add('card-body')
+    if(links !== null) {
+      const linkContainer = document.createElement("div")
+      linkContainer.classList.add("card-body")
       var i
-      for (i = 0; i < links.length; i++) {
-        const linkHref = links[i].getAttribute('href')
+      for(i = 0; i < links.length; i++) {
+        const linkHref = links[i].getAttribute("href")
         const linkText = links[i].innerHTML
-        const link = document.createElement('a')
-        link.setAttribute('href', linkHref)
-        link.classList.add('card-link')
+        const link = document.createElement("a")
+        link.setAttribute("href", linkHref)
+        link.classList.add("card-link")
         link.textContent = linkText
         linkContainer.appendChild(link)
       }
       container.appendChild(linkContainer)
     }
     // Buttons
-    if (buttons !== null) {
-      const buttonContainer = document.createElement('div')
-      buttonContainer.classList.add('card-body')
-      for (i = 0; i < buttons.length; i++) {
-        const buttonHref = buttons[i].getAttribute('href')
-        const buttonColor = buttons[i].getAttribute('color')
+    if(buttons !== null) {
+      const buttonContainer = document.createElement("div")
+      buttonContainer.classList.add("card-body")
+      for(i = 0; i < buttons.length; i++) {
+        const buttonHref = buttons[i].getAttribute("href")
+        const buttonColor = buttons[i].getAttribute("color")
         const buttonText = buttons[i].innerHTML
-        const button = document.createElement('a')
-        button.setAttribute('href', buttonHref)
-        button.classList.add('btn', 'btn-' + buttonColor)
+        const button = document.createElement("a")
+        button.setAttribute("href", buttonHref)
+        button.classList.add("btn", "btn-" + buttonColor)
         button.textContent = buttonText
         buttonContainer.appendChild(button)
       }
       container.appendChild(buttonContainer)
     }
     // Footer
-    if (footer !== null) {
-      const footerContainer = document.createElement('div')
-      footerContainer.classList.add('card-footer')
+    if(footer !== null) {
+      const footerContainer = document.createElement("div")
+      footerContainer.classList.add("card-footer")
       footerContainer.textContent = footer
       container.appendChild(footerContainer)
     }
     shadow.appendChild(container)
   }
 }
-customElements.define('bs-card', Card)
+customElements.define("bs-card", Card)
--- test/test.js
+++ test/test.js
@@ -4,25 +4,80 @@ class Card extends HTMLElement {
     const shadow = this.attachShadow({
       mode: "open"
     });
-    $("bs-card h4").attr("slot", "header");
-    $("bs-card img").attr("slot", "img");
+    $("bs-card h6").attr("slot", "body");
+    $("bs-card h6").addClass("card-subtitle");
+    $(this).children("p").attr("slot", "body");
+    $(this).children("p").addClass("card-text");
+    $(this).children("ul").attr("slot", "body");
+    $(this).children("ul").addClass("list-group list-group-flush");
+    $(this).children("ul").children("li").addClass("list-group-item");
+    $(this).children("a").attr("slot", "body");
+    $(this).children("a").addClass("card-link");
+
+    const headerText = $(this).children("h4").text();;
+    const imgSrc = $(this).children("img").attr("src");
+    const imgAlt = $(this).children("img").attr("alt");
+    const imgPosition = $(this).children("img").attr("pos");
+    const titleText = $(this).children("h5").text();
+    const footerText = $(this).children("footer").html();
+
+    // Set Stylesheet
     const style = document.createElement("link");
     style.setAttribute("rel", "stylesheet");
-    style.setAttribute("src", "../test/card.css");
+    style.setAttribute(
+      "src",
+      "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    );
     shadow.appendChild(style);
+    // Define other elements
+    // Main container
+    const containerStyle = this.getAttribute("style");
     const container = document.createElement("div");
     container.classList.add("card");
-    const header = document.createElement("slot");
-    header.setAttribute("name", "header");
-    container.appendChild(header);
-    const img = document.createElement("slot");
-    img.setAttribute("name", "img");
-    container.appendChild(img);
+    container.setAttribute("style", containerStyle);
+    // Header
+    const headerContainer = document.createElement("div");
+    headerContainer.classList.add("card-header");
+    headerContainer.innerHTML = headerText;
+    container.appendChild(headerContainer);
+    // Image
+    const imgContainer = document.createElement("img");
+    imgContainer.setAttribute("src", imgSrc);
+    imgContainer.setAttribute("alt", imgAlt);
+    // Body
     const body = document.createElement("div");
-    body.classList.add("card-body");
+    // Title
+    const titleContainer = document.createElement("h5");
+    titleContainer.classList.add("card-title");
+    titleContainer.textContent = titleText;
+    body.appendChild(titleContainer);
+    // Body Slot
     const slot = document.createElement("slot");
+    slot.setAttribute("name", "body");
+    slot.setAttribute("class", "card-body");
     body.appendChild(slot);
-    container.appendChild(body);
+    // Define Order of Elements
+    if(imgPosition == "top") {
+      body.classList.add("card-body");
+      imgContainer.classList.add("card-img-top");
+      container.appendChild(imgContainer);
+      container.appendChild(body);
+      //container.appendChild(footerContainer);
+    }
+    if(imgPosition == "bottom") {
+      body.classList.add("card-body");
+      imgContainer.classList.add("card-img-bottom");
+      container.appendChild(body);
+      container.appendChild(imgContainer);
+      //container.appendChild(footerContainer);
+    }
+    if(imgPosition == "overlay") {
+      body.classList.add("card-img-overlay");
+      imgContainer.classList.add("card-img");
+      container.appendChild(imgContainer);
+      container.appendChild(body);
+      //container.appendChild(footerContainer);
+    }
     shadow.appendChild(container);
   }
 }
